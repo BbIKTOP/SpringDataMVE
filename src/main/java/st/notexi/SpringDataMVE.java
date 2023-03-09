858f69aab5cb96eeaae97e3c73d16bb8bb0d3728
bbiktop <vik@notexi.st>
2023-03-09 03:01:48 +0200

    Insert/Update and Delete User

---
+++ src/main/java/st/notexi/SpringDataMVE.java
@@ -0,0 +1,13 @@
+package st.notexi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringDataMVE
+{
+    public static void main(String[] args)
+    {
+        SpringApplication.run(SpringDataMVE.class, args);
+    }
+}
